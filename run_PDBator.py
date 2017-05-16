367d68de815399f19ecb6b4d18e2018dd0f4fadf
zedrian <artsiom.kopats@gmail.com>
2017-05-16 11:51:01 +0300

    Added saving protein sequences to .csv.

--- run_PDBator.py
+++ run_PDBator.py
@@ -1,4 +1,12 @@
 import bio_utility, utility, clusterization
+from os.path import join
+
+def save_sequences(proteins, file_name):
+    with open(file_name, 'w') as file:
+        file.write('id,sequence\n')
+        for protein in proteins:
+            file.write('{0},{1}\n'.format(protein.id, protein.sequence))
+
 
 # Create folder for PDB structures and filling it with PDB files.
 folder_name = utility.folder_creation(utility.folder_path_generation())
@@ -11,5 +19,7 @@ bio_utility.saving_alignment_results(bio_utility.alignment_at_cluster_group(
 # Obtaining and writing down the results of alignment in the "size" cluster
 bio_utility.saving_alignment_results(bio_utility.alignment_at_cluster_group(
     clusterization.clusterisation_via_ligand_type(protein_list)), folder_name, 'ligand_type')
+
+save_sequences(protein_list, join(folder_name, 'sequences.csv'))
 
 print("All has been done, master!")
